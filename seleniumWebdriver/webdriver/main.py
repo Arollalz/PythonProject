6a9ce5e22c4589c52c131ff5381a6c8fab1e16cc
Arollalz <445223894@qq.com>
2016-06-07 20:53:37 +0800

    use WebDriverWait

--- seleniumWebdriver/webdriver/main.py
+++ seleniumWebdriver/webdriver/main.py
@@ -1,4 +1,15 @@
 from witkeyuser import Witkeyuser
+import os
+# f = open(os.getcwd()+"\selectWitkeyUserLoginNameAndPassword.csv")
+# print os.getcwd()
+# for line in f.readlines():
+#     print line
+#     name_password = line.split(',')
+#     print name_password
+#     user = Witkeyuser(name_password[0], name_password[1], 10000)
+#     user.checkTask()
+# f.close()
 
-user = Witkeyuser("15196775582", "123456")
+
+user = Witkeyuser("18780165474", "123456", 4)
 user.checkTask()
--- seleniumWebdriver/webdriver/witkeyuser.py
+++ seleniumWebdriver/webdriver/witkeyuser.py
@@ -2,8 +2,11 @@
 import time
 from selenium import webdriver
 import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 class Witkeyuser:
-    def __init__(self, name, password, task_limit_number=1):
+    def __init__(self, name, password, task_limit_number=2):
         self.name = name
         self.password = password
         self.task_limit_number = task_limit_number
@@ -14,23 +17,34 @@ class Witkeyuser:
         browser = webdriver.Chrome(chromedriver)
         browser.get("http://172.16.8.5:4031/")
         #go to login page
+        WebDriverWait(browser, 10).until(lambda the_driver: the_driver.find_element_by_xpath("/html/body/div[1]/div/div/nav/ul/li[2]").is_displayed())
         browser.find_element_by_xpath("/html/body/div[1]/div/div/nav/ul/li[2]").click()
-        time.sleep(3)
+
         #login with name and password
+        WebDriverWait(browser, 10).until(lambda the_driver: the_driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div[1]/input").is_displayed())
         browser.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div[1]/input").send_keys(self.name)
         browser.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div[2]/input").send_keys(self.password)
         browser.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[2]/div[3]/button").click()
-        time.sleep(3)
+
         #begin to check work
+        WebDriverWait(browser, 10).until(lambda the_driver: the_driver.find_element_by_xpath("/html/body/div[4]/div[2]/div[1]/div[3]/button/span").is_displayed())
         browser.find_element_by_xpath("/html/body/div[4]/div[2]/div[1]/div[3]/button/span").click()
-        time.sleep(5)
+
         #check
         finished_task_num = 0
+
         while finished_task_num < self.task_limit_number:
-            browser.find_element_by_xpath("/html/body/div[4]/div[2]/div[2]/div[3]/div/div[2]/div/div[1]/span[2]/span/ul/li[7]").click()
-            browser.find_element_by_id("questionSubmit").click()
-            time.sleep(0.5)
-            browser.find_element_by_id("questionSubmit").click()
-            finished_task_num += 1
-            time.sleep(0.5)
+            try:
+                WebDriverWait(browser, 10).until(lambda the_driver: the_driver.find_element_by_xpath("/html/body/div[4]/div[2]/div[2]/div[3]/div/div[2]/div/div[1]/span[2]/span/ul/li[7]").is_displayed())
+                browser.find_element_by_xpath("/html/body/div[4]/div[2]/div[2]/div[3]/div/div[2]/div/div[1]/span[2]/span/ul/li[7]").click()
+                browser.find_element_by_id("questionSubmit").click()
+                time.sleep(5)
+                WebDriverWait(browser, 10).until(lambda the_driver: EC.element_to_be_clickable(the_driver.find_element_by_id("questionSubmit")))
+                browser.find_element_by_id("questionSubmit").click()
+                finished_task_num += 1
+            except TimeoutException:
+                finished_task_num -= 1
+                print "timeout"
+        # ng - scope
+        # ng - isolate - scope
         browser.quit()
